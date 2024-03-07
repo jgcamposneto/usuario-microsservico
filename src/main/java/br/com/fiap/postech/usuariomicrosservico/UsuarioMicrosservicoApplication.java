1fb288cd0b2387bcb72749d25a61cd1ec25c851d
João Gabriel <jgcamposneto@gmail.com>
2024-03-07 14:58:56 -0300

    Habilitado Spring Security com autenticação baseada em memória

---
+++ src/main/java/br/com/fiap/postech/usuariomicrosservico/UsuarioMicrosservicoApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.postech.usuariomicrosservico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsuarioMicrosservicoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsuarioMicrosservicoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/postech/usuariomicrosservico/UsuarioMicrosservicoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.postech.usuariomicrosservico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioMicrosservicoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
